625b1a9822f4f65c621163e60ac2c8b7c11c7601
Rob Geada <rob@geada.net>
2023-03-30 17:16:29 +0100

    added alex script

---
+++ scripts/alex.sh
@@ -0,0 +1,10 @@
+echo "s/resources\/odh-minimal.yaml/${1}/g"
+sed "s&resources/odh-minimal.yaml&${1}&g" scripts/install.sh > scripts/alex_install.sh
+
+scripts/alex_install.sh
+scripts/deploy_models.sh
+scripts/send_data_batch.sh batch_01
+scripts/send_data_batch.sh batch_02
+scripts/register_metric_monitoring.sh
+
+rm scripts/alex_install.sh
